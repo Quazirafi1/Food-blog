aa80cf0651cf1b1e7947963b219d10e896770782
Quazirafi1 <55546196+Quazirafi1@users.noreply.github.com>
2019-10-24 10:08:28 +0600

    Reached visitor homepage

--- app.js
+++ app.js
@@ -2,12 +2,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var ejs = require('ejs');
-//var expSession = require('express-session');
-//var cookieParser = require('cookie-parser');
-//var login = require('./controllers/login');
-//var home = require('./controllers/home');
-//var logout = require('./controllers/logout');
-//var employee = require('./controllers/employee');
+var homepage = require('./controllers/homepage');
+var home = require('./controllers/home');
 var app = express();
 
 
@@ -17,12 +13,8 @@ app.set('view engine', 'ejs');
 
 //MIDDLEWARE
 app.use(bodyParser.urlencoded({extended:true}));
-//app.use(expSession({secret:'my top secret value', saveUninitialized:true, resave: false}));
-//app.use(cookieParser());
-//app.use('/login', login);
-//app.use('/home', home);
-//app.use('/employee', employee);
-//app.use('/logout', logout);
+app.use('/homepage', homepage);
+app.use('/home', home);
 
 
 //ROUTER
