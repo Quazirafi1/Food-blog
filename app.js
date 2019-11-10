9d2f22f352359fb0d990c7532d202343efcb1008
Quazi Ghulam Rafi <55546196+Quazirafi1@users.noreply.github.com>
2019-11-10 10:16:37 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+/* Importing Different Modules */
+
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const hbs = require('express-handlebars');
+const {mongoDbUrl, PORT} = require('./config/configuration');
+
+
+
+const app = express();
+
+
+// Configure Mongoose to Connect to MongoDB
+mongoose.connect(mongoDbUrl, { useNewUrlParser: true })
+    .then(response => {
+        console.log("MongoDB Connected Successfully.");
+    }).catch(err => {
+        console.log("Database connection failed.");
+});
+
+
+
+/* Configure express*/
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+/* Setup View Engine To Use Handlebars */
+app.engine('handlebars', hbs({defaultLayout: 'default'}));
+app.set('view engine' , 'handlebars');
+
+
+
+/* Routes */
+const defaultRoutes = require('./routes/defaultRoutes');
+const adminRoutes = require('./routes/adminRoutes');
+
+app.use('/', defaultRoutes);
+app.use('/admin', adminRoutes);
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
